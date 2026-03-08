b88071a67e6e021b92c831350ed9eda3810b48ce
kakunpc <15257475+kakunpc@users.noreply.github.com>
2026-03-08 21:11:20 +0900

    ビルドエラー修正

--- Editor/ManacoPass.cs
+++ Editor/ManacoPass.cs
@@ -66,7 +66,7 @@ namespace com.kakunvr.manaco
             UnityEngine.Object.DestroyImmediate(component);
         }
 
-        public Mesh ApplyEyeSubMesh(
+        internal Mesh ApplyEyeSubMesh(
             Manaco.EyeRegion region,
             SkinnedMeshRenderer smr,
             Material overrideMaterial = null,
